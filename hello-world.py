5e33e2bdf51091c54ada3622f391255f9bf94c9c
22dionse <49483278+22dionse@users.noreply.github.com>
2019-04-10 12:40:54 -0400

    Add files via upload
    Here ya go

---
+++ hello-world.py
@@ -0,0 +1,14 @@
+# Seth Dionne
+# 4/10/2019
+
+####################
+#    Hello World   #
+#                  # 
+# Our First Python # 
+#    Application   # 
+####################
+
+# print "Hello, world" to the scrren.
+print(
+    "Hello, world"
+)
